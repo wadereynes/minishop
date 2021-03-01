16e1bce63a11f6e18bd0d2f8c95b52691322519f
Wade <wadeshow25@gmail.com>
2021-03-01 23:44:08 +0800

    11-5

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGNUP = 'SIGNUP'
+export const LOGIN = 'LOGIN'
 
 export const signup = (email, password) => {
   return async (dispatch) => {
@@ -22,5 +23,30 @@ export const signup = (email, password) => {
     const resData = await response.json()
     console.log(resData)
     dispatch({ type: SIGNUP })
+  }
+}
+
+export const login = (email, password) => {
+  return async (dispatch) => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBH-rhyR8tHJ0b6jsCqVkkZeYQKgKinndo',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }),
+      }
+    )
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
+    const resData = await response.json()
+    console.log(resData)
+    dispatch({ type: LOGIN })
   }
 }
