aa461ef3adc2de730843d9adc6fbc81fd5aa78f1
Wade <wadeshow25@gmail.com>
2021-03-06 22:18:14 +0800

    15-12

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -111,7 +111,19 @@ const EditProductScreen = (props) => {
   }, [dispatch, prodId, formState])
 
   useEffect(() => {
-    props.navigation.setParams({ submit: submitHandler })
+    props.navigation.setOptions({
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title='Save'
+            iconName={
+              Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
+            }
+            onPress={submitHandler}
+          />
+        </HeaderButtons>
+      ),
+    })
   }, [submitHandler])
 
   const inputChangeHandler = useCallback(
@@ -194,21 +206,9 @@ const EditProductScreen = (props) => {
 }
 
 export const screenOptions = (navData) => {
-  const submitFn = navData.route.params ? navData.route.params.submit : null
   const routeParams = navData.route.params ? navData.route.params : {}
   return {
     headerTitle: routeParams.productId ? 'Edit Product' : 'Add Product',
-    headerRight: () => (
-      <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          title='Save'
-          iconName={
-            Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'
-          }
-          onPress={submitFn}
-        />
-      </HeaderButtons>
-    ),
   }
 }
 
