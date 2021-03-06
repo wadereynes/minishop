a46a5f573260101d6facc413a6db54b8410733e5
Wade <wadeshow25@gmail.com>
2021-03-06 22:12:22 +0800

    15-11

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -45,7 +45,7 @@ const EditProductScreen = (props) => {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState()
 
-  const prodId = props.navigation.getParam('productId')
+  const prodId = props.route.params ? props.route.params.productId : null
   const editedProduct = useSelector((state) =>
     state.products.userProducts.find((prod) => prod.id === prodId)
   )
@@ -194,11 +194,10 @@ const EditProductScreen = (props) => {
 }
 
 export const screenOptions = (navData) => {
-  const submitFn = navData.navigation.getParam('submit')
+  const submitFn = navData.route.params ? navData.route.params.submit : null
+  const routeParams = navData.route.params ? navData.route.params : {}
   return {
-    headerTitle: navData.navigation.getParam('productId')
-      ? 'Edit Product'
-      : 'Add Product',
+    headerTitle: routeParams.productId ? 'Edit Product' : 'Add Product',
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
